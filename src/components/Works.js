8482417fde3f897c0d39ebc27121c252463c0e8f
HeeJae Jeon <jeonheejae@HeeJaes-MacBook-Pro.local>
2022-02-05 23:59:13 +0900

    Works completed

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,39 +1,49 @@
-import React from 'react';
+import React from "react";
+import GSCaltexImg from "../GSCaltex.png";
+import HanwhaImg from "../HanwhaTechwin.png";
+import oneThirdFitnessImge from "../OneThirdFitness.png";
+import TAFSImg from "../TAFS.png"
+import Juniqe from "../Juniqe.png"
 
 const Works = () => {
     return (
         <div className="works-wrapper ">
             <div className="works-info border-bottom">
-                <div className="row featurette">
+                <div className="row featurette works-header">
                     <div className="col-md-6">
                         <h1>Featured works</h1>
                         <p className="lead">This is a collection of projects that are delivered recently. He has worked for from SMEs to corporates.
                             You will see how he got insights and how he solved user’s problems. Feel free to contact me if you have more questions on the projects.</p>
                     </div>
-                    <div className="col-md-6">
-                        <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="424" height="283" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="GSC: 424x283" preserveAspectRatio="xMidYMid slice" focusable="false"><title>GSC</title><rect width="100%" height="100%" fill="#eee"></rect></svg>
+                    <div className="col-md-6 works-imgs">
+                        <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" role="img" aria-label="GSC: 424x283" preserveAspectRatio="xMidYMid slice" focusable="false" src={GSCaltexImg} alt="GSC"></img>
                         <h3>GS Caltex</h3>
                     </div>
                 </div>
-                <div className="row featurette">
-                    <div className="col-md-6">
-                        <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="424" height="283" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="HanwhaTechWin: 424x283" preserveAspectRatio="xMidYMid slice" focusable="false"><title>HanwhaTechWin</title><rect width="100%" height="100%" fill="#eee"></rect></svg>
+                <div className="row featurette works-list">
+                    <div className="col-md-6 works-imgs">
+                        <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" role="img" aria-label="GSC: 424x283" preserveAspectRatio="xMidYMid slice" focusable="false" src={HanwhaImg} alt="Hanwha"></img>
                         <h3>Hanwha Techwin</h3>
                     </div>
-                    <div className="col-md-6">
-                        <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="424" height="283" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="1/3fitness: 424x283" preserveAspectRatio="xMidYMid slice" focusable="false"><title>1/3fitness</title><rect width="100%" height="100%" fill="#eee"></rect></svg>
+                    <div className="col-md-6 works-imgs">
+                        <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" role="img" aria-label="GSC: 424x283" preserveAspectRatio="xMidYMid slice" focusable="false" src={oneThirdFitnessImge} alt="1/3Fitness"></img>
                         <h3>1/3 Fitness</h3>
                     </div>
                 </div>
                 <h1 className='personalWorks'>Personal works</h1>
-                <div className="row featurette">
-                    <div className="col-md-6">
-                        <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="424" height="283" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="aTravelAppForSingapore: 424x283" preserveAspectRatio="xMidYMid slice" focusable="false"><title>aTravelAppForSingapore</title><rect width="100%" height="100%" fill="#eee"></rect></svg>
-                        <h3>A Travel App For Singapore</h3>
+                <div className="row featurette works-list">
+                    <div className="col-md-6 works-imgs">
+                        <a href="https://bootcamp.uxdesign.cc/finding-small-local-business-for-foreign-visitors-to-singapore-6d888d1d9e39">
+                            <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" role="img" aria-label="GSC: 424x283" preserveAspectRatio="xMidYMid slice" focusable="false" src={TAFSImg} alt="TAFS"></img>
+                            <h3>A Travel App For Singapore</h3>
+                        </a>
                     </div>
-                    <div className="col-md-6">
-                        <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="424" height="283" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Juniqe: 424x283" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Juniqe</title><rect width="100%" height="100%" fill="#eee"></rect></svg>
-                        <h3>Juniqe</h3>
+
+                    <div className="col-md-6 works-imgs">
+                        <a href="https://bootcamp.uxdesign.cc/revamping-juniqes-website-one-week-challenge-ed1a591e2377">
+                            <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" role="img" aria-label="GSC: 424x283" preserveAspectRatio="xMidYMid slice" focusable="false" src={Juniqe} alt="Juniqe"></img>
+                            <h3>Juniqe</h3>
+                        </a>
                     </div>
                 </div>
             </div>
