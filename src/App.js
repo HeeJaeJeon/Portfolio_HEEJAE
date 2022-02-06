b8612fca9d7dcda3da03f3f0c81f9a9c5f185cdd
HeeJaeJeon <juns880526@hotmail.com>
2022-02-06 16:49:57 +0900

    About-this-website completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import Works from "./components/Works";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Thiswebsite from "./components/Thiswebsite";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Works />
       <About />
       <Contact />
+      <Thiswebsite />
     </div>
   );
 }
---
+++ src/components/Thiswebsite.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Thiswebsite = () => {
+    return (
+        <div className="thisWebSite-wrapper">
+            <div className="thisWebSite-info">
+            <h1 className="thisWebSiteHeader col-md-12 thisWebSiteHeader">About this website</h1>
+            <p>In case you were wondering this site is:</p>
+            <ul>
+                <li>Designed and built on a Macbook Pro using Figma and Visual Studio Code</li>
+                <li>Developed by using HTML, CSS, Reactjs, Bootstrap</li>
+                <li>Hosted by “ “</li>
+                <li>Tracking data using “ “</li>
+                <li>Set in the wonderful Notosans and Playfair Display</li>
+            </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Thiswebsite;
